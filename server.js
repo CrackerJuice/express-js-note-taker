9826ba2118b6c377f3e68172e7790d1dd90b99f9
CrackerJuice <Alex.marie3976@gmail.com>
2024-07-29 00:32:44 -0700

    Started logic in server.js file

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const express = require('express');
+const apiRoutes = require('./public/routes/apiRoutes');
+const htmlRoutes = require('./public/routes/htmlRoutes');
+const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
